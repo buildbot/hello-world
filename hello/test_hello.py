3daddc871fe00e20d9ebc62d3039271ef9a2a423
Pierre Tardy <tardyp@gmail.com>
2017-09-05 10:36:52 +0200

    initial commit

---
+++ hello/test_hello.py
@@ -0,0 +1,20 @@
+"""sample test"""
+import unittest
+
+from hello import hello
+
+
+class TestHello(unittest.TestCase):
+    """sample test"""
+
+    def test_world(self):
+        """sample test"""
+        self.assertEqual(hello('world'), 'hello world')
+
+    def test_world_bytes(self):
+        """sample test with bytes"""
+        self.assertEqual(hello(b'world'), b'hello world')
+
+    def test_world_unicode(self):
+        """sample test with unicode"""
+        self.assertEqual(hello(u'world'), u'hello world')
