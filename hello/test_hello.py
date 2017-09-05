7f8e719e30f3f6b98558541d21dc8b589a4131be
Pierre Tardy <tardyp@gmail.com>
2017-09-05 16:04:50 +0200

    remove byte test. it does not work on py3

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -11,10 +11,6 @@ class TestHello(unittest.TestCase):
         """sample test"""
         self.assertEqual(hello('world'), 'hello world')
 
-    def test_world_bytes(self):
-        """sample test with bytes"""
-        self.assertEqual(hello(b'world'), b'hello world')
-
     def test_world_unicode(self):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
