a87ae6bea07a7f3256c89b9ad93ac4e4fb8ef7a1
Mikolaj Szczesny <miszczes0@gmail.com>
2023-09-04 14:09:29 +0200

    test commit for buildbot
    Signed-off-by: Mikolaj Szczesny <miszczes0@gmail.com>

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,6 +1,11 @@
 """hello world module"""
 
 
+def goodbye(who):
+    """function that says goodbye"""
+    return "bye bye " + who
+
+
 def hello(who):
     """function that greats"""
     return "hello " + who
--- hello/test_hello.py
+++ hello/test_hello.py
@@ -1,7 +1,7 @@
 """sample test"""
 import unittest
 
-from hello import hello
+from hello import hello, goodbye
 
 
 class TestHello(unittest.TestCase):
@@ -9,8 +9,11 @@ class TestHello(unittest.TestCase):
 
     def test_world(self):
         """sample test"""
-        self.assertEqual(hello('world'), 'hello world')
+        self.assertEqual(hello("world"), "hello world")
+
+    def test_bye(self):
+        self.assertEqual(goodbye("world"), "bye bye world")
 
     def test_world_unicode(self):
         """sample test with unicode"""
-        self.assertEqual(hello(u'world'), u'hello world')
+        self.assertEqual(hello("world"), "hello world")
